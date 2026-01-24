ce3b7901ef9179421a9a83bb9e610b5aaa0c8ba8
Jack <jacktotten@hotmail.co.uk>
2026-01-24 12:03:46 +0000

    Load event schedule by year method added to utilities

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .load_data_utils import load_session_data
+from .load_data_utils import load_session_data, load_event_schedule
 
--- utils/load_data_utils.py
+++ utils/load_data_utils.py
@@ -1,7 +1,7 @@
 import fastf1
 
 def load_event_schedule(year: int):
-    return fastf1.events.EventSchedule(year)
+    return fastf1.get_event_schedule(year, include_testing=False)
 
 def load_session_data(year, grand_prix, session_type):
     session = fastf1.get_session(year, grand_prix, session_type)
