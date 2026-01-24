7f1f8a3a5f2acc75ee2e0050c248d50e46b58672
Jack <jacktotten@hotmail.co.uk>
2026-01-24 12:47:48 +0000

    loading individual race data utilities

--- utils/load_data_utils.py
+++ utils/load_data_utils.py
@@ -3,6 +3,11 @@ import fastf1
 def load_event_schedule(year: int):
     return fastf1.get_event_schedule(year, include_testing=False)
 
+def load_race_event(year, event_name: str):
+    race = fastf1.get_event(year, event_name, 'R')
+    race.load(laps=False, telemetry=False, weather=False, messages=False)
+    return race
+
 def load_session_data(year, grand_prix, session_type):
     session = fastf1.get_session(year, grand_prix, session_type)
     session.load()
