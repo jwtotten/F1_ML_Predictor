ede02fcbb5d4f1e35a4c29b719df0f75138e9f83
Jack <jacktotten@hotmail.co.uk>
2026-01-31 12:20:00 +0000

    Loading race data (driver abbreviations, points and positions) for given race event

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from .load_data_utils import load_session_data, load_event_schedule
+from .load_data_utils import load_session_data, load_event_schedule, load_race_event
 
--- utils/load_data_utils.py
+++ utils/load_data_utils.py
@@ -4,9 +4,17 @@ def load_event_schedule(year: int):
     return fastf1.get_event_schedule(year, include_testing=False)
 
 def load_race_event(year, event_name: str):
-    race = fastf1.get_event(year, event_name, 'R')
+    race = fastf1.get_session(year, event_name, 'R')
     race.load(laps=False, telemetry=False, weather=False, messages=False)
-    return race
+    race_data = {}
+    for _, driver_row in race.results.iterrows():
+        abbreviation, race_points, race_position = (
+            driver_row["Abbreviation"],
+            driver_row["Points"],
+            driver_row["Position"],
+        )
+        race_data[abbreviation] = {"Points": race_points, "Position": race_position}
+    return race_data
 
 def load_session_data(year, grand_prix, session_type):
     session = fastf1.get_session(year, grand_prix, session_type)
